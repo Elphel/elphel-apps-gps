5865b30795eb243d0b298e2de08c1d07195d866a
Oleg Dzhimiev <oleg@elphel.com>
2017-02-27 12:22:13 -0700

    correct license header

--- src/read_imu_log.php
+++ src/read_imu_log.php
@@ -1,12 +1,24 @@
 <?php
-/*
- * FILE NAME  : read_imu_log.php
- * DESCRIPTION: parse imu log from Elphel 10393 (and 10353)
- * VERSION: 1.0
- * AUTHOR: Oleg K Dzhimiev <oleg@elphel.com>
- * LICENSE: AGPL, see http://www.gnu.org/licenses/agpl.txt
- * Copyright (C) 2016 Elphel, Inc.
- */
+/**
+ * @file read_imu_log.php
+ * @brief parse imu log from Elphel 10393 (and 10353)
+ * @copyright Copyright (C) 2017 Elphel Inc.
+ * @author Elphel Inc. <support-list@support.elphel.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
  
 set_time_limit(0);
 
